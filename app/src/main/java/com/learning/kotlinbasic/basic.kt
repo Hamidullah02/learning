223b479763a0d4de47f70a1fc4e35d008385def9
Hamidullah02 <kazihamidullah1@gmail.com>
2024-08-21 14:44:13 +0600

    added something 2nd time

--- app/src/main/java/com/learning/kotlinbasic/basic.kt
+++ app/src/main/java/com/learning/kotlinbasic/basic.kt
@@ -1,11 +1,18 @@
 package com.learning.kotlinbasic
 
 fun main() {
+
     for(i in 1..10) {
         println("press 1-3 to play")
         var opt = arrayOf("ROCK", "PAPER", "SISSORs")
         println("1.ROCK \n2.PAPER \n3.SISSORs")
+
         var user = readln().toInt()
+        while (!(user<=1 && user >=3)){
+            println("enter cprrect number")
+            user = readln().toInt()
+        }
+
         var computer = (1..3).random()
         println("user : ${opt[user - 1]}")
         println("computer : ${opt[computer - 1]}")
@@ -18,11 +25,9 @@ fun main() {
             user == 3 && computer == 2 -> "player"
             else ->"computer"
         }
+        if(winner=="tie") println("it was a TIE")
+        else if(winner=="player") println("you won")
+        else println("you lost")
 
-//        if (user == computer) println("draw")
-//        else if (user == 1 && computer == 2)  println("you lose")
-//        else if (user == 2 && computer == 3) println("you lose")
-//        else if (user == 3 && computer == 1) println("you lose")
-//        else println("you won")
     }
 }
